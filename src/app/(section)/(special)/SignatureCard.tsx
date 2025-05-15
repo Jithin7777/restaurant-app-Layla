4bf59a3606697176edcf728948109740241ad834
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-05-16 00:07:52 +0530

    add signature special section wtin images

---
+++ src/app/(section)/(special)/SignatureCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface SignatureCardProps {
+  image: string;
+  title: string;
+  price: number;
+}
+
+const SignatureCard: React.FC<SignatureCardProps> = ({
+  image,
+  title,
+  price,
+}) => {
+  return (
+    <div className="flex flex-col items-center text-center text-white">
+      <div className="relative w-full h-[180px] md:h-[200px]   overflow-hidden">
+        <img
+          src={image}
+          alt={title}
+          className="w-[320px] h-full object-cover object-center"
+          style={{ objectPosition: "center center" }}
+        />
+      </div>
+      {/* Price and Title */}
+      {/* <div className="absolute top-12 -right-5 bg-yellow-600 text-black font-bold text-sm px-4 py-2 rounded-full shadow-lg">
+        € {price}
+      </div> */}
+      <h3 className="mt-2 font-serif text-xl font-playfair font-normal whitespace-pre-line">
+        {title}
+      </h3>
+    </div>
+  );
+};
+
+export default SignatureCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Hero from './(section)/Hero'
 import ExploreMenu from './(section)/ExploreMenu'
 import AboutUs from './(section)/AboutUs'
 import Specials from './(section)/Specials'
+import SignatureSpecial from './(section)/(special)/SignatureSpecial'
 
 const page = () => (
   <main>
@@ -13,6 +14,7 @@ const page = () => (
       <ExploreMenu />
       <AboutUs />
       <Specials />
+      <SignatureSpecial/>
     </div>
   </main>
 )
